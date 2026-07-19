574772023c6da1b644a65df26cfe0c30fbcf8a2f
JDE-Projects <289071709+JDE-Projects@users.noreply.github.com>
2026-07-19 11:44:20 -0500

    Bump version to v1.4.0

--- simple_sftp_client.py
+++ simple_sftp_client.py
@@ -38,7 +38,7 @@ from urllib.request import Request, urlopen
 import webview
 import paramiko
 
-APP_VERSION = "1.3.0"
+APP_VERSION = "1.4.0"
 GITHUB_REPO = "JDE-Projects/Simple-SFTP-Client"   # owner/repo for update checks
 
 # Weak / deprecated / CVE-prone algorithms we refuse (secure-or-fail).
